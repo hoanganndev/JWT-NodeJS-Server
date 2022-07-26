e73f8ca290a6eeee235a7e7f78c8a026c4ede4d3
hoanganndev <hoangann.dev@gmail.com>
2022-07-26 14:08:36 +0700

    Create roles and assign permissions to each group with group-roles

--- src/routes/api.js
+++ src/routes/api.js
@@ -3,6 +3,8 @@ import groupController from "../controller/groupController";
 import loginRegisterController from "../controller/loginRegisterController";
 import userController from "../controller/userController";
 import { checkUserJWT, checkUserPermission } from "../middleware/JWTAction";
+import rolesController from "../controller/rolesController";
+import groupRolesController from "../controller/groupRolesController";
 const router = express.Router();
 
 /**
@@ -16,17 +18,35 @@ const initApiRoutes = app => {
     // Router register and login
     router.post("/register", loginRegisterController.handleRegister);
     router.post("/login", loginRegisterController.handleLogin);
+    router.get("/logout", loginRegisterController.handleLogout);
 
     // Router CRUD users
-    router.get("/user/read", userController.readUsersFunction);
     router.post("/user/create", userController.createUserFunction);
+    router.get("/user/read", userController.readUsersFunction);
     router.put("/user/update", userController.updateUserFunction);
     router.delete("/user/delete", userController.deleteUserFunction);
 
     // Router get current account user
     router.get("/account", userController.getUserAccountFunction);
+
     // Router get groups
     router.get("/group/read", groupController.readGroupsFunction);
+
+    // Router roles
+    router.post("/role/create", rolesController.createRolesFunction);
+    router.get("/role/read", rolesController.readRolesFunction);
+    router.put("/role/update", rolesController.updateRoleFunction);
+    router.delete("/role/delete", rolesController.deleteRoleFunction);
+
+    // Router Group with Roles
+    router.get(
+        "/roles/by-group/:groupId",
+        groupRolesController.getRolesByGroupFunction
+    );
+    router.post(
+        "/roles/assign-to-group",
+        groupRolesController.assignRolesToGroupFunction
+    );
 
     return app.use("/api/v1/", router);
 };
